977786952788a0feb438ff9ae71c3c93da3666ad
thirdy <thirdy.derivera@gmail.com>
2016-02-27 20:18:39 +0800

    learned js scopes the hard way, always use var keyword otherwise it will become global

--- search/js/es.js
+++ search/js/es.js
@@ -14,12 +14,12 @@ $.ajax({
       //parse your data here
       //you can split into lines using data.split('\n') 
       //an use regex functions to effectively parse it
-      lines = data.split('\n');
+      var lines = data.split('\n');
       for(i in lines) {
-          line = lines[i];
+          var line = lines[i];
           if(line.trim().length > 0 && !line.startsWith(';')) {
-              lastEqualsIdx =  line.indexOf("=");
-              regex = line.substring(0, lastEqualsIdx).trim();
+              var lastEqualsIdx = line.indexOf("=");
+              var regex = line.substring(0, lastEqualsIdx).trim();
               terms[regex] = line.substring(lastEqualsIdx + 1).trim();
           }
       }
@@ -27,37 +27,70 @@ $.ajax({
 });
 
 function parseSearchInput(input) {
-    tokens = input.split(" ");
-    result = [];
+    var tokens = input.split(" ");
+    var queryTokens = [];
+    console.info("tokens=" + tokens);
     for (i in tokens) {
-        token = tokens[i];
-        if ( token != "OR" || token != "AND") {
-            for (regex in terms) {
-               if (terms.hasOwnProperty(regex)) {
-                rgex = new RegExp(regex);
-                foundMatch = rgex.test(token);
-                if (foundMatch) {
-                    result.push(terms[regex]);
-                    break;
-                }
-              }
+        var token = tokens[i];
+        var evaluatedToken = token;
+        if ( token != "OR" && token != "AND" ) {
+            evaluatedToken = evalSearchTerm(token);
+            console.info(token + "=" + evaluatedToken);
+            if (evaluatedToken && hasBackTick(evaluatedToken)) {
+                evaluatedToken = parseSearchInput(evaluatedToken);
             }
-        } else {
-            result.push(token);
         }
+        queryTokens.push(evaluatedToken);
     }
-    resultStr = result.join(" ");
-    return resultStr;
+    var queryString = queryTokens.join(" ");
+    console.info("queryString=" + queryString);
+    return queryString;
+}
+
+function evalSearchTerm(token) {
+    var result = "";
+    for (regex in terms) {
+       if (terms.hasOwnProperty(regex)) {
+        var rgex = new RegExp('^' + regex + '$');
+        var foundMatch = rgex.test(removeParensAndBackTick(token));
+        if (foundMatch) {
+            result = terms[regex];
+            if (hasOpenParen(token))  result = '(' + result;
+            if (hasCloseParen(token)) result = result + ')';
+            break;
+        }
+      }
+    }
+    return result;
+}
+
+function removeParensAndBackTick(token) {
+    var _token = token.replace(/[\(\)`]/g, "");
+    return _token;
+}
+
+function hasOpenParen(token) {
+    return token.startsWith('(');
+}
+
+function hasCloseParen(token) {
+    return token.endsWith(')');
+}
+
+function hasBackTick(token) {
+    return token.indexOf('`') != -1;
 }
 
 EsConnector.controller('ExileToolsHelloWorld', function($scope, es) {
   // Default
-  $scope.searchInput = "gloves OR boots";
+  $scope.searchInput = "2haxe";
+  $scope.queryString = "";
   // attributes.baseItemType:Armour AND shop.updated:>1456191110199
   $scope.doSearch = function() {
           console.log(terms);
-          searchQuery = parseSearchInput($scope.searchInput);
-          console.log(searchQuery);
+          var searchQuery = parseSearchInput($scope.searchInput);
+          console.log("searchQuery=" + searchQuery);
+          $scope.queryString = searchQuery;
           
           es.search({
           index: 'index',
